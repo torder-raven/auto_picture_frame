608e0a1917bd3801980df19fdb3709d40e2e97c9
kite-torder <jjy5@torder.com>
2024-03-06 12:57:38 +0900

    refactor: 이미지 위젯 생성 기능 함수화

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -18,9 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     Timer.periodic(Duration(seconds: timerUnit), (timer) {
       int? nextPage = pageController.page?.toInt();
-
       if (nextPage == null) {
-        // 페이지 값이 없을때
         return;
       }
 
@@ -30,7 +28,8 @@ class _HomeScreenState extends State<HomeScreen> {
         nextPage++;
       }
 
-      pageController.animateToPage(nextPage, duration: Duration(milliseconds: 500), curve: Curves.ease);
+      pageController.animateToPage(nextPage,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
     });
   }
 
@@ -38,46 +37,23 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: [
-            PageView(
-              controller: pageController,
-              children: [1, 2, 3]
-                  .map(
-                    (number) => Image.asset(
-                      'asset/img/banner$number.png',
-                      fit: BoxFit.fitWidth,
-                    ),
-                  )
-                  .toList(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red, // 원하는 색상으로 설정
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.pause, color: Colors.white), // 아이콘 색상 지정
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 40,
-                    decoration: BoxDecoration(color: Colors.red),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text('1'), Text('/3')],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
+        child: PageView(
+          controller: pageController,
+          children: buildImageWidgets([1, 2, 3]),
         ),
       ),
     );
+  }
+
+  // 이미지 위젯 생성 함수
+  List<Widget> buildImageWidgets(List<int> numbers) {
+    return numbers
+        .map(
+          (number) => Image.asset(
+            'asset/img/banner$number.png',
+            fit: BoxFit.fitWidth,
+          ),
+        )
+        .toList();
   }
 }
